5f476834142cf841f83c0711372b7e3040e51c91
JanGursky <jgursk@gmail.com>
2024-02-23 13:21:34 +0100

    migration fixed

--- database/migrations/2024_02_23_092249_create_owners_table.php
+++ database/migrations/2024_02_23_092249_create_owners_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('owners', function (Blueprint $table) {
             $table->bigIncrements('id')->unsigned();
+            $table->string ('first_name')->nullable();
             $table->string('surname')->nullable();
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
